fbb09342df11f5ca6712c38bc6d62c76858c48a2
randomstring0 <sandshowcase@gmail.com>
2025-10-16 16:50:47 +0700

    Create qwerty

---
+++ qwerty5.lua
@@ -0,0 +1,18 @@
+-- Subscribe to @Im_Patrick Right now!
+
+-- This script works with any accessory you are currently wearing.
+-- Want to wear multiple hats? Watch this tutorial:
+-- https://www.youtube.com/results?search_query=how+to+wear+multiple+hats+on+mobile
+
+-- If the console shows “clown” and no GUI appears, your executor might not be supported.
+
+-- Your executor must support the "replicatesignal" function. If it doesn’t, the script will use a backup method called "loop respawn" that makes you keep respawning.
+-- When your hat falls off or disappears, the script will try to bring it back by respawning your character.
+-- Avoid lag, or it might not work properly.
+
+-- If you’re playing a game called “just/ a baseplate.”, type “-pd” before running this script.
+-- That game uses for older method fe script (from 2021), so this script may behave differently there.
+-- Command list for that game:
+-- https://raw.githubusercontent.com/randomstring0/jab/refs/heads/main/cmdlist.txt
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/qwertys/refs/heads/main/qwerty4.lua"))()
