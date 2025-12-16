6c99600f80f625a4c51b506419e410f5c8bc8c69
randomstring0 <sandshowcase@gmail.com>
2025-12-16 20:25:42 +0700

    Create qwerty

---
+++ qwerty11.lua
@@ -0,0 +1,32 @@
+-- noob rig id (paid): 139904067056008, 108186273151388, 72292903231768, 84451219120140, 95290698984301, 14254923976
+-- white rig (paid): 140395948277978, 82942681251131, 102599402682100, 90960046381276, 138364679836274
+
+-- fedora rig (free): 3033910400,3409612660,3438342658,3398308134,4324158403,3822880197,4154538250,3443038622,4819740796
+
+-- gun (paid): 17163596085
+-- gun (free): 5552252553
+
+--bullet (paid): 4504231783
+
+getgenv().Configuration = {
+    ["allowshiftlock"] = true;
+    ["ctrltp"] = false;
+    ["placeholders"] = true;
+    ["clickfling"] = false;
+    ["highlightflingtargets"] = true;
+    ["discharscripts"] = false;
+    ["flingchangestate"] = 3;
+    ["respawntp"] = 3;
+    ["breakjointsmethod"] = 1;
+    ["simrad"] = true;
+    ["hidedeatheffect"] = true;
+    ["permadeath"] = true;
+}
+
+-- script by sedeecake
+-- reanimate : fdless by myworld
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/qwertys/refs/heads/main/qwerty10.lua"))()
+
+
+-- converted by : im_patrick
