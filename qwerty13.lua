0cdc16102068316b31e3e759c2177c10d7ab7dd9
randomstring0 <sandshowcase@gmail.com>
2026-01-25 08:17:59 +0700

    Create qwerty

---
+++ qwerty13.lua
@@ -0,0 +1,11 @@
+-- r15 katana
+
+--recomended katana (layered clothing):
+--https://www.roblox.com/catalog/12711048235/Katana-of-Oppressed
+--https://www.roblox.com/catalog/12380877175/Katana-Handheld
+--https://www.roblox.com/catalog/14045118788/Holdable-Katana
+--https://www.roblox.com/catalog/13327913614/Katana-of-The-Serpent
+--https://www.roblox.com/catalog/14784238015/Holdable-Katana
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/qwertys/refs/heads/main/qwerty12.lua"))()
